1005d862e8180aa00605e7410d24b9664405c6cd
walilentin <grebenukvalentin@icloud.com>
2024-09-13 12:42:42 +0300

    update metadata settings

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,8 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
