25d5d0e27f55619650bfae6156e982dc63c4a66e
walilentin <grebenukvalentin@icloud.com>
2024-09-17 13:24:51 +0300

    create id int pm mixin

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -14,5 +14,3 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
-
-    id: Mapped[int] = mapped_column(primary_key=True)
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,15 +1,11 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from .base import Base
+from ..mixins.id_int_pm import IdIntMixin
 
 
-class User(Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
-
-    __table_args__ = (
-        UniqueConstraint("foo", "bar"),
-    )
+class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[int]):
+    pass
