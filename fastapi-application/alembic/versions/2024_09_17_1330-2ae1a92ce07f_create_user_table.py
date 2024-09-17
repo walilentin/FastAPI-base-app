e840dc2717245521d0b83dc5074faf36df007074
walilentin <grebenukvalentin@icloud.com>
2024-09-17 13:36:28 +0300

    migrations

---
+++ fastapi-application/alembic/versions/2024_09_17_1330-2ae1a92ce07f_create_user_table.py
@@ -0,0 +1,38 @@
+"""create user table
+
+Revision ID: 2ae1a92ce07f
+Revises: 
+Create Date: 2024-09-17 13:30:03.649692
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+revision: str = "2ae1a92ce07f"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "user",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=320), nullable=False),
+        sa.Column("hashed_password", sa.String(length=1024), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.Column("is_verified", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_user")),
+    )
+    op.create_index(op.f("ix_user_email"), "user", ["email"], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_user_email"), table_name="user")
+    op.drop_table("user")
