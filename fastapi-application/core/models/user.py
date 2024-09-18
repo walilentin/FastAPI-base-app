b72e1d80aac6d591b1ba546feeacd8cdd908ceb8
walilentin <grebenukvalentin@icloud.com>
2024-09-18 11:23:04 +0300

    update user model

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,14 +1,17 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from typing import TYPE_CHECKING
 
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from .base import Base
+from .types.user_id import UserIDType
 from ..mixins.id_int_pm import IdIntMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
-class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[int]):
+
+class User(Base, IdIntMixin, SQLAlchemyBaseUserTable[UserIDType]):
+    pass
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyBaseUserTable(session, User)
+        return SQLAlchemyBaseUserTable(session, cls)
