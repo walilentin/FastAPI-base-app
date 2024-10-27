608a988e473a6682f5c4419bddf02b8379d31e34
Valentyn <grebenukvalentin@icloud.com>
2024-10-27 23:13:27 +0200

    add redirect

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from starlette.responses import RedirectResponse
 
 from api import router as api_router
 from core.config import settings
@@ -11,7 +12,7 @@ from core.models.db_helper import db_helper
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # startup
+
     yield
     # shutdown
     print("Dispose engine")
@@ -22,9 +23,17 @@ main_app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
 main_app.include_router(
     api_router,
 )
+
+
+@main_app.get("/")
+async def redirect_to_auth():
+    return RedirectResponse(url="/api/v1/pages")
+
+
 if __name__ == "__main__":
     uvicorn.run(
         "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
